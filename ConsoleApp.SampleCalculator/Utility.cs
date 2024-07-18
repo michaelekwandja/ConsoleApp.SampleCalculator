996b8beedb8c757d6a440c3954a1b33cce8647a2
michaelekwandja <145231541+michaelekwandja@users.noreply.github.com>
2024-07-18 12:13:02 +0200

    Classes

---
+++ ConsoleApp.SampleCalculator/Utility.cs
@@ -0,0 +1,49 @@
+﻿
+
+
+public class Utility
+{
+    public static int choice { get; set; }
+    public  static int num1 { get; set; }
+    public static int num2 { get; set; }
+    public static int AddNumbers(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public static int SubtractNumbers(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+
+    public static int Product(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+
+    public static int Quotient(int num1, int num2)
+    {
+        return num1 / num2;
+    }
+
+    public static int Fibonnaci(int num1, int num2)
+    {
+        var answer = 0;
+        for (int i = num1; i <= num2; i++)
+        {
+            answer += i;
+        }
+        return answer;
+    }
+    public static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("******** - Welcome to the sample calculator! - ********");
+        Console.WriteLine("Please select an operation (-1 to exit program) ");
+        Console.WriteLine("1. Addition");
+        Console.WriteLine("2. Subtraction");
+        Console.WriteLine("3. Multiplication");
+        Console.WriteLine("4. Division");
+        Console.WriteLine("5. Fibonacci sequence");
+    }
+}
